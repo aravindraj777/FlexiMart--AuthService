95a9564007abb8eb14a7a4c6e4361d20698d333e
Aravind <aravindmunna240@gmail.com>
2024-12-08 19:15:36 +0530

    custom userdetails

---
+++ src/main/kotlin/com/flexiMart/auth_service/config/CustomUserDetails.kt
@@ -0,0 +1,38 @@
+package com.flexiMart.auth_service.config
+
+import com.flexiMart.auth_service.domain.User
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+data class CustomUserDetails(private val user: User):UserDetails {
+    override fun getAuthorities(): Collection<GrantedAuthority> {
+       return listOf(SimpleGrantedAuthority(user.role?.name ?: "ROLE_DEFAULT"))
+    }
+
+    override fun getPassword(): String {
+       return user.password
+    }
+
+    override fun getUsername(): String {
+        return user.email
+    }
+
+    override fun isAccountNonExpired(): Boolean {
+        return false
+    }
+
+
+    override fun isAccountNonLocked(): Boolean {
+        return false
+    }
+
+    override fun isCredentialsNonExpired(): Boolean {
+        return false
+    }
+
+    override fun isEnabled(): Boolean {
+        return true
+    }
+
+}
